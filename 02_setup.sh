4752ad26671c202ad7586b28a1ac32a6b4352bf3
chizuchizu <ochimasumi@gmail.com>
2021-05-06 21:51:26 +0900

    first-commit

---
+++ 02_setup.sh
@@ -0,0 +1,6 @@
+# nvim
+ln -sf ~/Git/dotfiles/nvim/dein.toml ~/.config/nvim/dein.toml
+ln -sf ~/Git/dotfiles/nvim/dein_lazy.toml ~/.config/nvim/dein_lazy.toml
+ln -sf ~/Git/dotfiles/nvim/init.vim ~/.config/nvim/init.vim
+
+ln -sf ~/Git/dotfiles/tmux/.tmux.conf ~/.tmux.conf
