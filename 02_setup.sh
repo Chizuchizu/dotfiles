98e7f936ae89824bbf79040badaec334e1fa96c5
chizuchizu <ochimasumi@gmail.com>
2021-05-06 22:31:12 +0900

    oh-my-zsh

--- 02_setup.sh
+++ 02_setup.sh
@@ -8,4 +8,5 @@ ln -sf ~/github/dotfiles/nvim/init.vim ~/.config/nvim/init.vim
 ln -sf ~/github/dotfiles/tmux/.tmux.conf ~/.tmux.conf
 
 # zsh
+ln -sf ~/github/dotfiles/zsh/oh-my-zsh.sh ~/.oh-my-zsh/oh-my-zsh.sh
 ln -sf ~/github/dotfiles/zsh/.zshrc ~/.zshrc
